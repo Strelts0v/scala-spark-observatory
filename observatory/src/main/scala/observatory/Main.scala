0668602d93249fb390a501ef636190abaf68f27b
ICX\hleb.straltsou <hleb.straltsou@itechart-group.com>
2019-06-25 18:21:26 +0300

    Update Main.scala with naive data loading

--- observatory/src/main/scala/observatory/Main.scala
+++ observatory/src/main/scala/observatory/Main.scala
@@ -1,8 +1,23 @@
 package observatory
 
+import observatory.util.ParsingUtils
 import org.apache.log4j.{Level, Logger}
 
 object Main extends App {
 
   Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+
+  val firstYear = 1975
+  val lastYear = 2016
+
+  println("Loading stations...")
+  val stations = ParsingUtils.parseStationsFile("/stations.csv")
+  println("Loading of stations has been accomplished!")
+  println(s"Stations total size: ${stations.size}")
+  val temperatures = (firstYear until lastYear) map { year => {
+    println(s"Loading file with temperatures for year: $year")
+    (year, ParsingUtils.parseTemperaturesFile(s"/$year.csv"))
+  }}
+
+  println(s"Temperatures total size: ${temperatures.length}")
 }
