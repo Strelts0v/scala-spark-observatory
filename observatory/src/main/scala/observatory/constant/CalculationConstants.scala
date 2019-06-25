5970630e64e97b83894dff7e437f088f51ca8aa0
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2019-06-25 20:49:30 +0300

    Add CalculationConstants.scala

---
+++ observatory/src/main/scala/observatory/constant/CalculationConstants.scala
@@ -0,0 +1,12 @@
+package observatory.constant
+
+import scala.math.Pi
+
+object CalculationConstants {
+
+  val inverseDistanceWeightingPower = 3.0
+  val minArcDistance = 1.0
+  val radianConversionRate = Pi / 180.0
+  val earthRadius = 6371.0
+
+}
