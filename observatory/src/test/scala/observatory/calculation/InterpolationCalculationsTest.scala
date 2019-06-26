c7b4e9625e35e1d7677b9c073eb19ab02619ff9d
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2019-06-26 19:56:37 +0300

    Add tests for InterpolationCalculations.scala

--- observatory/src/test/scala/observatory/calculation/InterpolationCalculationsTest.scala
+++ observatory/src/test/scala/observatory/calculation/InterpolationCalculationsTest.scala
@@ -1,13 +1,15 @@
 package observatory.calculation
 
+import observatory.constant.CalculationConstants._
 import observatory.Location
-
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalacheck.Prop._
 
 import observatory.data.testVisualizationData._
+
 
 @RunWith(classOf[JUnitRunner])
 class InterpolationCalculationsTest extends FunSuite with GeneratorDrivenPropertyChecks {
@@ -20,7 +22,7 @@ class InterpolationCalculationsTest extends FunSuite with GeneratorDrivenPropert
   }
 
   test("Check commutativity law with random generated locations") {
-    forAll (locationSample, locationSample) { (l1: Location, l2: Location) =>
+    forAll (locationGen, locationGen) { (l1: Location, l2: Location) =>
       assert(InterpolationCalculations.greatCircleDistance(l1, l2) === InterpolationCalculations.greatCircleDistance(l2, l1))
     }
   }
@@ -31,6 +33,39 @@ class InterpolationCalculationsTest extends FunSuite with GeneratorDrivenPropert
 
   test("Check calculation of Great Circle Distance for London and Paris locations") {
     assert(InterpolationCalculations.greatCircleDistance(londonLocation, parisLocation) - 344.0 < 0.2)
+  }
+
+  test("Check symmetric work of inverseDistanceWeighting algorithm") {
+    val sample: List[(Location, Double)] =
+      List(
+        (Location(30, 0), 5.0),
+        (Location(0, 30), 5.0),
+        (Location(-30, 0), 5.0),
+        (Location(0, -30), 5.0)
+      )
+    val loc = Location(0, 0)
+
+    assert(InterpolationCalculations.inverseDistanceWeighting(sample, loc, inverseDistanceWeightingPower) === 5.0)
+  }
+
+  test("Check work of inverseDistanceWeighting algorithm with particular points") {
+    val sample: List[(Location, Double)] =
+      List(
+        (Location(30, 0), 10.0),
+        (Location(0, 30), 5.0),
+        (Location(-30, 0), 15.0),
+        (Location(0, -30), 20.0)
+      )
+    val loc = Location(-30, 0)
+
+    assert(InterpolationCalculations.inverseDistanceWeighting(sample, loc, inverseDistanceWeightingPower) === 15.0)
+  }
+
+  test("Check work of inverseDistanceWeighting algorithm with random samples") {
+    check(forAll (sampleGen, locationGen) { (sample: List[(Location, Double)], loc: Location) => {
+      val result = InterpolationCalculations.inverseDistanceWeighting(sample, loc, inverseDistanceWeightingPower)
+      (result <= 50.0) && (result >= -50.0)
+    }})
   }
 
 }
