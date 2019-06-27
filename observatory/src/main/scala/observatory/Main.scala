03671cdcd34b9c599b4bdc5226d5fbf25e87360a
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2019-06-27 19:56:10 +0300

    Add poor implementation of building tempretures map image (but it works!)

--- observatory/src/main/scala/observatory/Main.scala
+++ observatory/src/main/scala/observatory/Main.scala
@@ -1,23 +1,58 @@
 package observatory
 
+import observatory.Extraction.{locateTemperatures, locationYearlyAverageRecords}
 import observatory.util.ParsingUtils
 import org.apache.log4j.{Level, Logger}
 
 object Main extends App {
 
-  Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+//  Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+//
+//  val firstYear = 1975
+//  val lastYear = 2016
+//
+//  println("Loading stations...")
+//  val stations = ParsingUtils.parseStationsFile("/stations.csv")
+//  println("Loading of stations has been accomplished!")
+//  println(s"Stations total size: ${stations.size}")
+//  val temperatures = (firstYear until lastYear) map { year => {
+//    println(s"Loading file with temperatures for year: $year")
+//    (year, ParsingUtils.parseTemperaturesFile(s"/$year.csv"))
+//  }}
+//
+//  println(s"Temperatures total size: ${temperatures.length}")
 
-  val firstYear = 1975
-  val lastYear = 2016
+  val FIRST_YEAR = 1975
+  val LAST_YEAR = 2015
 
-  println("Loading stations...")
-  val stations = ParsingUtils.parseStationsFile("/stations.csv")
-  println("Loading of stations has been accomplished!")
-  println(s"Stations total size: ${stations.size}")
-  val temperatures = (firstYear until lastYear) map { year => {
-    println(s"Loading file with temperatures for year: $year")
-    (year, ParsingUtils.parseTemperaturesFile(s"/$year.csv"))
-  }}
+  val colours: List[(Double, Color)] = List(
+    (60.0, Color(255, 255, 255)),
+    (32.0, Color(255, 0, 0)),
+    (12.0, Color(255, 255, 0)),
+    (0.0, Color(0, 255, 255)),
+    (-15.0, Color(0, 0, 255)),
+    (-27.0, Color(255, 0, 255)),
+    (-50.0, Color(33, 0, 107)),
+    (-60.0, Color(0, 0, 0))
+  )
 
-  println(s"Temperatures total size: ${temperatures.length}")
+  // println("Loading stations")
+  // val stations = Extraction.parseStationsFile("/stations.csv")
+  // println("Loading done")
+  // println(stations.size)
+
+  // TODO Convert temps and stations data to yearly data
+  /*
+  val temps: Map[Int, Iterable[(Location, Double)]] = {
+    for {
+      year <- FIRST_YEAR until LAST_YEAR
+      println(s"Loading year $year")
+    } yield (year, locationYearlyAverageRecords(locateTemperatures(year, "/stations.csv", s"/$year.csv")))
+  }.toMap
+  */
+
+  val temps1975 = locationYearlyAverageRecords(locateTemperatures(1975, "/stations.csv", "/1975.csv"))
+  val image = Visualization.visualize(temps1975, colours)
+  image.output("./map_1975.png")
+
 }
