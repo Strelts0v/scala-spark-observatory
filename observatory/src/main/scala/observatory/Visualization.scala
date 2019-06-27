97f4fee0d764e697d466f7f7e24544a8524ffd59
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2019-06-27 18:59:00 +0300

    Add implementation of interpolateColor method

--- observatory/src/main/scala/observatory/Visualization.scala
+++ observatory/src/main/scala/observatory/Visualization.scala
@@ -24,8 +24,29 @@ object Visualization {
     * @param value The value to interpolate
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
-  def interpolateColor(points: Iterable[(Temperature, Color)], value: Temperature): Color = {
-    ???
+  def interpolateColor(points: Iterable[(Double, Color)], value: Double): Color = {
+    val sortedPoints = points.toList.sortWith(_._1 < _._1).toArray
+    interpolateColorAlgo(sortedPoints, value)
+  }
+
+  def interpolateColorAlgo(sortedPoints: Array[(Double, Color)], value: Double): Color = {
+    for (i <- 0 until sortedPoints.length - 1) {
+      (sortedPoints(i), sortedPoints(i + 1)) match {
+        case ((v1, Color(r1, g1, b1)), (v2, Color(r2, g2, b2))) => {
+          if (v1 > value) v1
+          else if (v2 > value) {
+            val ratio = (value - v1) / (v2 - v1)
+            return Color(
+              (r1 + (r2 - r1) * ratio).toInt,
+              (g1 + (g2 - g1) * ratio).toInt,
+              (b1 + (b2 - b1) * ratio).toInt
+            )
+          }
+        }
+      }
+    }
+    // In case when value is not within the colour map. Return maximum color
+    sortedPoints(sortedPoints.length-1)._2
   }
 
   /**
