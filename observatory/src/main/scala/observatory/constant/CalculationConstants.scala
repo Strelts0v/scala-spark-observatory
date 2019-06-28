4e6d89b2f201129fae9f90893b2aa70a7889f906
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2019-06-28 10:25:16 +0300

    Update value of inverseDistanceWeightingPower

--- observatory/src/main/scala/observatory/constant/CalculationConstants.scala
+++ observatory/src/main/scala/observatory/constant/CalculationConstants.scala
@@ -4,7 +4,7 @@ import scala.math.Pi
 
 object CalculationConstants {
 
-  val inverseDistanceWeightingPower = 3.0
+  val inverseDistanceWeightingPower = 4.0
   val minArcDistance = 1.0
   val radianConversionRate = Pi / 180.0
   val earthRadius = 6371.0
