00d6f44242a71b2867401315b762caab53b26c84
hleb.straltsou <gleb.streltsov.4by@gmail.com>
2019-06-28 10:24:49 +0300

    Add TileVisualizer.scala

---
+++ observatory/src/main/scala/observatory/visualization/TileVisualizer.scala
@@ -0,0 +1,21 @@
+package observatory.visualization
+
+import observatory.Interaction.tileLocation
+import observatory.{Color, Location, Tile}
+
+import scala.math.pow
+
+class TileVisualizer(colors: Iterable[(Double, Color)], tile: Tile) extends Visualizer {
+  val alpha = 127
+  val width = 256
+  val height = 256
+  val colorMap: Array[(Double, Color)] = colors.toList.sortWith(_._1 < _._1).toArray
+
+  // Tile offset is in the zoom+8 coordinate system
+  val x0: Int = pow(2.0, tile.zoom + (8 - 1)).toInt * tile.x
+  val y0: Int = pow(2.0, tile.zoom + (8 - 1)).toInt * tile.y
+
+  def xyToLocation(x: Int, y: Int): Location = {
+    tileLocation(Tile(tile.zoom + 8, x0 + x, y0 + y))
+  }
+}
